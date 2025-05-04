727575ebf3bad7da8f6ddb05d0c1d62247b02495
cory <csz8115@rit.edu>
2025-05-04 16:56:12 -0400

    fixed config

--- app.ts
+++ app.ts
@@ -26,6 +26,10 @@ app.use(cookieParser()); // Add cookie parser middleware
 // Logger middleware
 app.use(pinoHttp({ logger }));
 
+// User routes
+app.use('/api/v1/member', userRoutes);
+app.use('/api/v1/admin', adminRoutes);
+app.use('/api/v1/officer', officerRoutes);
 // auth middleware 
 app.use(async (req, res, next) => {
   // Check if the req is coming from login or register
@@ -44,11 +48,6 @@ app.use(async (req, res, next) => {
     }
   }
 });
-
-// User routes
-app.use('/api/v1/member', userRoutes);
-app.use('/api/v1/admin', adminRoutes);
-app.use('/api/v1/officer', officerRoutes);
 
 // Serve React App for all non-API routes
 // app.use(express.static(path.join(__dirname, "./dist")));
